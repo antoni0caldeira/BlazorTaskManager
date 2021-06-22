fa9cabeda650be6ec7ccfe1bc63dde7b6e746a59
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-22 17:56:12 +0100

    Add project files.

---
+++ BlazorTaskManager/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorTaskManager.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTaskManager.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
