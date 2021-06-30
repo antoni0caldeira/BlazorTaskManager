72f164fc3085f40fee017ea9bb990d58e14fc909
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-30 10:09:37 +0100

    Tasks Repository

---
+++ TaskManagerDAL/Repositories/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagerDAL.Data;
+using TaskManagerDAL.Repositories.Interfaces;
+
+namespace TaskManagerDAL.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private TaskManagerDbContext taskManagerDbContext;
+
+        public TaskRepository(TaskManagerDbContext taskManagerDbContext)
+        {
+            this.taskManagerDbContext = taskManagerDbContext;
+        }
+
+        IEnumerable<Task> ITaskRepository.GetAll()
+        {
+            return (IEnumerable<Task>)taskManagerDbContext.Tasks.ToList();
+        }
+    }
+}
