1b87f56631328b95c7df509e8f5255310b18f5b7
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-30 10:13:16 +0100

    Add DTO

---
+++ TaskManager.Business/Dtos/TaskDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TaskManagerDAL.Models;
+
+namespace TaskManager.Business.Dtos
+{
+    class TaskDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public DateTimeOffset EndDate { get; set; }
+
+        public TaskStatus Status { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
